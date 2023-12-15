65f1697357eac026ddf45a983968bbc93574ecb2
jihun <jihunpark.b@gmail.com>
2023-12-15 17:26:33 +0900

    Set basic classes.

---
+++ GuiManager.py
@@ -0,0 +1,3 @@
+
+class GuiManager():
+    pass
---
+++ jsonManager.py
@@ -0,0 +1,18 @@
+
+class JsonManager:
+    def __init__(self):
+        pass
+
+    def LoadData(self):
+        pass
+
+    def SaveData(self):
+        pass
+
+    #TODO: 프로젝트 관리(수정/추가/삭제)
+    def AddProject(self, project):
+        pass
+    def DeleteProject(self, project):
+        pass
+    def EditProject(self, project):
+        pass
