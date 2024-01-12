7631621542150bf1268be463b22b07028cdbdf0b
jihun <jihunpark.b@gmail.com>
2024-01-12 14:34:44 +0900

    Set icon and create exe file.

--- GuiManager.py
+++ GuiManager.py
@@ -1,6 +1,7 @@
 import sys 
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 from Manager import Manager
 
 class GuiManager(QWidget):
@@ -8,6 +9,7 @@ class GuiManager(QWidget):
     def __init__(self):
         super().__init__()
         self.manager = Manager()
+        self.setWindowIcon(QIcon('icon.png'))
         self.resize(600,300)
 
         self.cb_project = self.CreateCombo(self.manager.GetProjectList(),None)
